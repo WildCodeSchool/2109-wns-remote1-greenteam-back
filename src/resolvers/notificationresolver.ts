c99c56efd66ba2cdc379669f4485812cbbadc7e8
NoraLef <nora.lefeuvre@gmail.com>
2022-02-23 18:11:56 +0100

    Notification resolver cleanup

--- src/resolvers/notificationresolver.ts
+++ src/resolvers/notificationresolver.ts
@@ -7,26 +7,13 @@ import Notification from '../entity/Notification';
 import User from '../entity/User';
 
 /*
-=> Get a notification by id
 => Get all notifications by user
 => Create a notification
 => Delete a notification
-=> Update a notification
 */
 
 @Resolver(Notification)
 export default class NotificationResolver {
-  @Query((returns) => Notification)
-  getOneNotification(@Arg('id', type => ID) id: string) {
-    const notificationRepository: Repository<Notification> =
-      getRepository(Notification);
-    const notification = notificationRepository
-      .createQueryBuilder('notification')
-      .where('notification.id = :id', { id })
-      .getOne();
-    return notification;
-  }
-
   @Query((returns) => [Notification])
   getAllNotificationsByUser(@Arg('user', (returns) => User) user: User) {
     const notificationRepository: Repository<Notification> =
@@ -66,26 +53,5 @@ export default class NotificationResolver {
       .where('notification.id = :id', { id })
       .getOne();
     return notificationRepository.remove(await notification);
-  }
-
-  @Mutation((returns) => Notification)
-  updateNotification(
-    @Arg('id') id: number,
-    @Arg('user', (returns) => User) user: User,
-    @Arg('title') title: string,
-    @Arg('description') description: string
-  ) {
-    const notificationRepository: Repository<Notification> =
-      getRepository(Notification);
-    const notification = notificationRepository
-      .createQueryBuilder('notification')
-      .where('notification.id = :id', { id })
-      .getOne();
-    return notificationRepository.save({
-      ...notification,
-      user,
-      title,
-      description,
-    });
   }
 }
