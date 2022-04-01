7e1c7589a94398e17eaaa115cfd88c30fc257fd7
Anojan Mylvaganam <m.anojan@live.fr>
2022-04-01 04:25:23 +0200

    adding docker scripts to build and start mysql

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
     project: './tsconfig.json',
-    tsconfigRootDir: './',
+    tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
